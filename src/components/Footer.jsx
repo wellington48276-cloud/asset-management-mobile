adedf4428351eb73b9a64e0852f1419f14c59e52
wellington48276-cloud <wellington.48276@santanadeparnaiba.sp.gov.br>
2026-07-31 15:55:19 -0300

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="app-footer">
-      <span>
-        SANTANA DE PARNAÍBA BY VITOR/NEEMIAS DIREITOS RESERVADOS • Sistema de Rastreamento de Ativos Criptografados v2.6.0
-      </span>
-    </footer>
-  );
-}
