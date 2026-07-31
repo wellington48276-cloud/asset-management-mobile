bf76286e5be04e0f4a956e84e97b9a273897fdcf
wellington48276-cloud <wellington.48276@santanadeparnaiba.sp.gov.br>
2026-07-31 16:20:48 -0300

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="app-footer">
-      <span>
-        SANTANA DE PARNAÍBA BY VITOR/NEEMIAS DIREITOS RESERVADOS • Sistema de Monitoramento de Ativos Inteligentes v3.0
-      </span>
-    </footer>
-  );
-}
--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import { Boxes, History } from 'lucide-react';
-export default function Header({ historyCount, onOpenHistory }) {
-  return <header className="topbar">
-    <div className="brand"><Boxes size={25}/><div><h1>INTEGRAÇÃO E TRATAMENTO DE PATRIMÔNIO</h1><span>Monitoramento inteligente de ativos</span></div></div>
-    <button className="icon-button" onClick={onOpenHistory} aria-label="Abrir histórico"><History size={21}/>{historyCount > 0 && <b>{historyCount}</b>}</button>
-  </header>;
-}
