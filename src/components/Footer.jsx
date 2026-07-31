06c190dce1487a7ff5110d8a7e25aa8dc527ffeb
wellington48276-cloud <wellington.48276@santanadeparnaiba.sp.gov.br>
2026-07-31 15:16:17 -0300

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,3 +0,0 @@
-export default function Footer({ usuarioAtual }) {
-  return <footer className="app-footer"><span>Sistema de Gestão Patrimonial</span>{usuarioAtual && <span className="footer-user">Operador: {usuarioAtual}</span>}</footer>;
-}
