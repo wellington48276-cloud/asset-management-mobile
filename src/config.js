cccbdec472d89632eff634d2486d76a215b38e6a
Vitor <wellington.48276@santanadeparnaiba.sp.gov.br>
2026-08-04 09:14:57 -0300

    Delete src directory

--- src/config.js
+++
@@ -1,7 +0,0 @@
-export const APP_VERSION = '1.1.3';
-export const EXPECTED_API_VERSION = '1.1.2';
-export const SESSION_STORAGE_KEY = 'patrimonio_session_v2';
-export const ACTIVE_SERVICE_KEY = 'patrimonio_servico_ativo_v2';
-export const MAX_IMAGE_EDGE = 2000;
-export const JPEG_QUALITY = 0.85;
-export const SYNC_INTERVAL_MS = 60_000;
